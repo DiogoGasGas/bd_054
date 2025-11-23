834193859a1b7bb518c089519e3e10bdafead1e3
João Miguel Neves Guiomar <fc62179@alunos.fc.ul.pt>
2025-11-23 19:51:51 +0000

    ok

--- ver_benchmarks.sql
+++ ver_benchmarks.sql
@@ -20,8 +20,9 @@ ORDER BY
     query_name,
     CASE 
         WHEN TRIM(etapa) = 'antes' THEN 1
-        WHEN TRIM(etapa) = 'depois indices' THEN 2
-        WHEN TRIM(etapa) = 'depois indices e otimizacao' THEN 3
-        ELSE 4
+        WHEN TRIM(etapa) = 'depois_s_seqscan' THEN 2
+        WHEN TRIM(etapa) = 'depois_c_seqscan' THEN 3
+        WHEN TRIM(etapa) = 'depois indices e otimizacao' THEN 4
+        ELSE 5
     END;
 
