b2f8ea7d418ca25a38ccc0594121653cb2aef1e1
Diogo Gaspar <diogommgaspar@gmail.com>
2025-12-04 18:22:05 +0000

    tem de ser tudo na mesma linha

--- migracao.ps1
+++ migracao.ps1
@@ -1,24 +1,18 @@
-# migracao.ps1
-
-$PG_HOST = "appserver.alunos.di.fc.ul.pt"
-$PG_USER = "bd054"
-$PG_DB   = "bd054"
-$MONGO_COLL = "funcionarios_migrados" 
-$MONGO_URI = "mongodb://bd054:bd054@appserver.alunos.di.fc.ul.pt:27017/bd054?authSource=bd054"
-
 Write-Host "--- A INICIAR MIGRACAO COMPLETA ---" -ForegroundColor Cyan
 
+# 0. LIMPEZA (Opcional mas recomendado: apaga o ficheiro antigo para evitar erros)
+if (Test-Path "dados_final.json") { Remove-Item "dados_final.json" }
+
 # 1. EXPORTAR
-# Removemos o '-o' daqui. O psql corre o script, e o script cria o ficheiro.
 Write-Host "1. A extrair dados complexos do PostgreSQL..."
-psql -h $PG_HOST -U $PG_USER -d $PG_DB -f exportar_dados.sql
+psql -h appserver.alunos.di.fc.ul.pt -U bd054 -d bd054 -f exportar_dados.sql
 
 # Verifica se o ficheiro foi criado
 if (Test-Path "dados_final.json") {
     
     # 2. IMPORTAR
-    Write-Host "2. A importar para a colecao '$MONGO_COLL' no MongoDB..."
-    mongoimport --uri $MONGO_URI --collection $MONGO_COLL --file dados_final.json --drop
+    Write-Host "2. A importar para a colecao 'funcionarios_migrados' no MongoDB..."
+    mongoimport --uri "mongodb://bd054:bd054@appserver.alunos.di.fc.ul.pt:27017/bd054?authSource=bd054" --collection funcionarios_migrados --file dados_final.json --drop
 
     Write-Host "--- SUCESSO! MIGRACAO CONCLUIDA ---" -ForegroundColor Green
 } else {
