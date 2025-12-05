74e6f1530a0a606d86d42a53169f7d15f494876f
Mariana <mariana.d.ferreira.2005@gmail.com>
2025-12-05 18:34:27 +0000

    coisas

---
+++ drop_idx.mongodb.js
@@ -0,0 +1,12 @@
+
+// ==============================
+// 1️⃣ Apagar todos os índices exceto _id
+// Coleção: funcionarios
+db.funcionarios.dropIndexes()
+
+// Coleção: departamentos
+db.departamentos.dropIndexes()
+
+// Coleção: vagas
+db.vagas.dropIndexes()
+
