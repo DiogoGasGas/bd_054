9c29d5ce2c45f1a8d36bb890d82a8a27fc3d33ec
Mariana <mariana.d.ferreira.2005@gmail.com>
2025-12-16 18:41:19 +0000

    tyuio

---
+++ drop_idx.mongodb.js
@@ -0,0 +1,12 @@
+
+// ==============================
+//  Apagar todos os índices exceto _id
+// Coleção: funcionarios
+db.funcionarios.dropIndexes()
+
+// Coleção: departamentos
+db.departamentos.dropIndexes()
+
+// Coleção: vagas
+db.vagas.dropIndexes()
+
